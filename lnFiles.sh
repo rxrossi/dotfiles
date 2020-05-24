11694ed501829a3dd9a0c50bc05ba5d052599568
Alex Ignez <rxrossi@Alexs-Air.lan>
2020-05-24 12:00:43 +0100

    Add tmux

--- lnFiles.sh
+++ lnFiles.sh
@@ -5,6 +5,9 @@ ln -s -f ~/dotfiles/karabiner.json ~/.config/karabiner/karabiner.json
 ln -s -f ~/dotfiles/zshrc ~/.zshrc
 ln -s -f ~/dotfiles/zshenv ~/.zshenv
 
+# TMUX
+ln -s -f ~/dotfiles/tmux.conf ~/.tmux.conf
+
 # Neovim
 mkdir ~/.config/nvim/
 ln -s -f ~/dotfiles/nvim/ ~/.config

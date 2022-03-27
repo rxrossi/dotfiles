4b60006d643599b4ef475f0124508abb6cc24605
Alex Ignez <rxrossi@Alexs-MacBook-Pro.local>
2022-03-27 12:30:48 +0100

    Add cspell

--- nvim/lua/config/null-ls.lua
+++ nvim/lua/config/null-ls.lua
@@ -2,12 +2,16 @@ local null_ls = require("null-ls")
 
 -- register any number of sources simultaneously
 local sources = {
-	null_ls.builtins.formatting.prettier,
+	null_ls.builtins.formatting.prettierd,
 	null_ls.builtins.formatting.eslint_d,
 	null_ls.builtins.formatting.stylua,
 
 	null_ls.builtins.diagnostics.write_good,
 	null_ls.builtins.diagnostics.eslint,
+	null_ls.builtins.diagnostics.cspell.with({
+		-- https://www.npmjs.com/package/cspell-dict-pt-br
+		extra_args = { "-c", vim.fn.expand("~/dotfiles/nvim/cspell.json"), "--locale", "en-GB,pt-BR" },
+	}),
 
 	null_ls.builtins.code_actions.gitsigns,
 	null_ls.builtins.code_actions.eslint,

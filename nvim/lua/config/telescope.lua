60fc8b8991e39ac40be403416b9b8ffec9821716
Alex Ignez <rxrossi@Alexs-MacBook-Pro.local>
2022-03-29 23:26:30 +0100

    Update telescope config

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -2,8 +2,13 @@ require("telescope").setup({
 	defaults = {
 		layout_strategy = "vertical",
 		layout_config = { vertical = { width = 0.999, height = 0.999 } },
+		theme = "dropdown",
 	},
 	pickers = {
+		builtin = {
+			previewer = false,
+			theme = "dropdown",
+		},
 		find_files = {
 			previewer = false,
 			theme = "dropdown",
@@ -24,7 +29,7 @@ require("telescope").setup({
 vim.cmd([[
   nnoremap <leader><leader> <cmd>Telescope <cr>
   nnoremap <leader>ff <cmd>Telescope find_files find_command=rg,--hidden,--files<cr>
-  nnoremap <leader>fg <cmd>:lua require("telescope").extensions.live_grep_raw.live_grep_raw()<cr>
+  nnoremap <leader>fg <cmd>:lua require("telescope").extensions.live_grep_raw.live_grep_raw({theme = "default"})<cr>
   nnoremap <leader>fb <cmd>Telescope buffers<cr>
   nnoremap <leader>fh <cmd>Telescope command_history<cr>
   nnoremap <leader>fc <cmd>Telescope commands<cr>

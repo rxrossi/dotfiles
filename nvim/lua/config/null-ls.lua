5e37ebc4ebaa0c81087d06b5817ff274f51d5376
Alex Ignez <rxrossi@Alexs-MacBook-Pro.local>
2022-03-29 13:19:50 +0100

    Remove write_good from diagnostics

--- nvim/lua/config/null-ls.lua
+++ nvim/lua/config/null-ls.lua
@@ -6,7 +6,7 @@ local sources = {
 	null_ls.builtins.formatting.eslint_d,
 	null_ls.builtins.formatting.stylua,
 
-	null_ls.builtins.diagnostics.write_good,
+	-- null_ls.builtins.diagnostics.write_good,
 	null_ls.builtins.diagnostics.eslint,
 	null_ls.builtins.diagnostics.cspell.with({
 		-- https://www.npmjs.com/package/cspell-dict-pt-br

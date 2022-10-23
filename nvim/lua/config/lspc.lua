d00be9c5b6e2f2f0ff4a2e0e0c8e7637c46b9630
Alex <rx.rossi@gmail.com>
2022-10-23 20:04:11 +0100

    Replace a deprecated function

--- nvim/lua/config/lspc.lua
+++ nvim/lua/config/lspc.lua
@@ -130,7 +130,7 @@ cmp.setup({
 })
 
 -- Setup lspconfig.
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 local lsp_installer = require("nvim-lsp-installer")
 

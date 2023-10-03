5eade7d9a892011f3e0a9af37e03ddf7700c9c9b
Alex Ignez <rx.rossi@gmail.com>
2023-10-03 10:38:00 +0100

    Update stuff

--- nvim/init.lua
+++ nvim/init.lua
@@ -318,6 +318,7 @@ vim.keymap.set('n', '<space>f', vim.lsp.buf.format, { desc = 'Format the file' }
 -- [[ Configure LSP ]]
 --  This function gets run when an LSP connects to a particular buffer.
 local on_attach = function(client, bufnr)
+  -- vim.o.foldmethod = 'indent'
   -- NOTE: Remember that lua is a real programming language, and as such it is possible
   -- to define small helper and utility functions so you don't have to repeat yourself
   -- many times.

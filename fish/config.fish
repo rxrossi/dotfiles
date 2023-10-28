17a2b2aaff8e5b3257ea639e732293ed5c2c8e64
Alex Ignez <alexignez@Alexs-MacBook-Pro.local>
2023-10-28 13:44:50 +0100

    Update setup instructions

---
+++ fish/config.fish
@@ -0,0 +1,12 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+fish_vi_key_bindings
+# fish_default_key_bindings
+
+alias v="nvim"
+
+# set -U fish_greeting
+
+set -gx PATH /Library/PostgreSQL/16/bin $PATH
